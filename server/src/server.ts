89d7a3adf83f6ba5eaf97e80710475ef412401ac
neelabhra-de <deneelabhra@gmail.com>
2026-09-24 13:49:10 +0530

    feat: improved performance

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,5 @@
 import app from './app.js'
 import { env } from './config/env.js'
-app.listen(env.port, () => console.log(`APEX F1 Express API listening on http://localhost:${env.port}`))
+app.listen(env.port, '0.0.0.0', () =>
+  console.log(`APEX F1 Express API listening on port ${env.port}`)
+)
